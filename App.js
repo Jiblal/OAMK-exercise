d8ba8fd14a30c1b9ba68431ad3f8bd0737f79737
jiblal <basantpaudel@gmail.com>
2022-01-19 12:44:18 +0200

    image added

--- App.js
+++ App.js
@@ -1,11 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import { useState } from 'react';
+import { StyleSheet, Text, View, Image, Button } from 'react-native';
 
 export default function App() {
+  const [luckyNumberImage, setLuckyNumberImage]= useState(require("./assets/dice-images/smiley.png"))
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.header}>Lucky Number</Text>
+      <Text style={styles.status}>First throw the dice to get your luky number</Text>
+      <Button title='STEP 1. THROW LUCKY NUMBER'/>
+      <Image style={styles.dice}/>
+      <Text style={styles.status}>then throw the dice to get the win number</Text>
+      <Button title='STEP 2. THROW WINNING NUMBER'/>
+      <Image style={styles.dice}/>
+      <Text style={styles.status}>To see if your are lucky</Text>
+      <Button title='STEP 2. THROW WINNING NUMBER'/>
     </View>
   );
 }
@@ -17,4 +26,18 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  header:{
+    fontSize:30,
+    marginBottom:15
+  },
+  dice:{
+    marginTop:15,
+    width:80,
+    height:80,
+    marginBottom: 15
+  },
+  status:{
+    fontSize:15,
+    marginBottom: 25
+  }
 });
